f7ef1d7c76236319c18e1596f71b3098f580d130
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-05-03 13:48:55 +0200

    Added a h2 elemnt tothe movie-list

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -20,7 +20,8 @@
           <button type="button" class="btn btn-success"  @click="addMovie">Lägg till film</button>
         </form>
 
-
+        <hr>
+        <h2>Dina filmer</h2>
         <ul id="movie-list">
           <li v-for="(movie, index) in movies" :key="index">
             {{ movie.title }} - 
