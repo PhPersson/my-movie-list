f07d99c2645d3cd0535d4422424a966c5aab990e
Philip Persson <51906227+PhPersson@users.noreply.github.com>
2023-05-03 09:58:59 +0200

    Implemted the addMovie function

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -6,10 +6,10 @@
         <form>
           <div class="form-group">
             <label for="title">Titel</label>
-            <input type="input" class="form-control" id="title" placeholder="Titeln på filmen här...">
+            <input type="input" v-model="title" class="form-control" id="title" placeholder="Titeln på filmen här...">
           </div>
           <label for="rating">Betyg</label>
-            <select class="form-control" id="rating" >
+            <select class="form-control" id="rating" v-model.number="rating" >
               <option>1</option >
               <option>2</option>
               <option>3</option>
@@ -17,12 +17,13 @@
               <option>5</option>
             </select>
         <br>
-          <button type="button" class="btn btn-success">Lägg till film</button>
+          <button type="button" class="btn btn-success"  @click="addMovie">Lägg till film</button>
         </form>
 
 
         <ul id="movie-list">
-          <li >
+          <li v-for="(movie, index) in movies" :key="index">
+            {{ movie.title }} - 
             <button id="removeBtn" class="btn btn-danger">Ta bort</button>
           </li>
         </ul>
@@ -38,8 +39,30 @@
 export default {
     data() {
         return {
+          // Först måste vi ha en lista [] där alla filmer skall sparas i. Varje film i listan behöver också ha en titel och ett betyg. 
+          movies: [],
+          title: '',
+          rating: '',
         }
     },
+
+    methods: {
+      addMovie() {
+        if (!this.title) {
+          alert("Fyll i titel!");
+          return;
+        }
+        if (!this.rating) {
+          alert("Fyll i betyg!");
+          return;
+        }
+        this.movies.push({ title: this.title, rating: this.rating });
+        this.title = '';
+        this.rating = '';
+
+      },
+    }
+
 }
 
 </script>
